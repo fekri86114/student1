b3626f3d94685a9d5e2a46801b18406d3f627a48
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-03-29 09:56:09 -0700

    insert and show student is done

---
+++ src/main/kotlin/info/fekri/spring1/controller/StudentController.kt
@@ -0,0 +1,40 @@
+package info.fekri.spring1.controller
+
+import com.google.gson.Gson
+import info.fekri.spring1.model.Student
+import info.fekri.spring1.repository.StudentRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class StudentController {
+    private lateinit var studentRepository: StudentRepository
+
+    @Autowired
+    fun setStudentRepository(studentRepo: StudentRepository) {
+        studentRepository = studentRepo
+    }
+
+    @GetMapping("/student")
+    fun getAllStudents(): ResponseEntity<Iterable<Student>> {
+        val dataFromDatabase = studentRepository.findAll() // returns the all the students
+        return ResponseEntity.ok(dataFromDatabase)
+    }
+
+    @PostMapping("/student")
+    fun insertStudent(
+        @RequestBody data: String
+    ): ResponseEntity<String> {
+        val gson = Gson()
+        val newStudent = gson.fromJson<Student>(data, Student::class.java)
+
+        studentRepository.save(newStudent) // save student
+
+        return ResponseEntity.ok("Success")
+    }
+
+}
--- src/main/kotlin/info/fekri/spring1/repository/StudentRepository.kt
+++ src/main/kotlin/info/fekri/spring1/repository/StudentRepository.kt
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface StudentRepository: CrudRepository<Student, String> {
+interface StudentRepository : CrudRepository<Student, String> {
 
 
 
