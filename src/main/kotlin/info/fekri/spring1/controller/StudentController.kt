a6feb3e2fc73024f091bcd625c6af6d3fa9402bb
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-03-30 06:49:59 -0700

    done

--- src/main/kotlin/info/fekri/spring1/controller/StudentController.kt
+++ src/main/kotlin/info/fekri/spring1/controller/StudentController.kt
@@ -5,6 +5,7 @@ import info.fekri.spring1.model.Student
 import info.fekri.spring1.repository.StudentRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -30,27 +31,29 @@ class StudentController {
     @PostMapping("/student")
     fun insertStudent(
         @RequestBody data: String
-    ): ResponseEntity<String> {
+    ) {
         val gson = Gson()
         val newStudent = gson.fromJson<Student>(data, Student::class.java)
 
         studentRepository.save(newStudent) // save student
-
-        return ResponseEntity.ok("Success")
     }
 
     @PutMapping("/student/updating{name}")
     fun updateStudent(
         @PathVariable("name") name: String,
         @RequestBody data: String
-    ): ResponseEntity<String> {
+    ) {
         val gson = Gson()
         val newStudent: Student = gson.fromJson(data, Student::class.java)
 
         studentRepository.save(newStudent)
+    }
 
-        println(name)
-        return ResponseEntity.ok("Success")
+    @DeleteMapping("/student/deleting{name}")
+    fun deleteStudent(
+        @PathVariable("name") name: String
+    ) {
+        studentRepository.deleteById(name)
     }
 
 }
