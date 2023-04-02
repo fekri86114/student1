d416cbe319f899d836408b2c0df75de2ee523113
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-04-02 01:50:39 -0700

    added room database and dao

---
+++ app/src/main/java/info/fekri/student1/model/local/student/Student.kt
@@ -0,0 +1,19 @@
+package info.fekri.student1.model.local.student
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@Entity(tableName = "student")
+data class Student(
+
+    @PrimaryKey
+    val name: String,
+
+    val course: String,
+
+    val score: Int
+
+) : Parcelable
